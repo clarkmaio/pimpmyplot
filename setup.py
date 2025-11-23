97ad8b121089cba0b6802f2f86ed9025650b2659
clarkmaio <maioliandrea0@gmail.com>
2025-11-23 22:05:32 +0100

    minor

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup, find_packages
 
-from pimpmyplot._version import __version__
+
+
+from setuptools import setup, find_packages
 
 with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()
